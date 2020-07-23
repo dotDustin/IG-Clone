f23fd3baf41e3a96f3c8b0fe560a8fbab4d6f146
dotDustin <logins.dmahone@gmail.com>
2020-07-23 00:34:11 -0400

    firebase login setup

--- IGClone/ViewController.swift
+++ IGClone/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Methods
+    
+    func signUpInErrorAlert(message: String) {
+        let alert = UIAlertController(title: "Sign In/Sign Up Error", message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Actions
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        guard emailTextField.text != "" else { signUpInErrorAlert(message: "email empty")
+            return
+        }
+        guard passwordTextField.text != "" else { signUpInErrorAlert(message: "password empty")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authData, error) in
+            
+            if error != nil {
+                print("error")
+                self.signUpInErrorAlert(message: error?.localizedDescription ?? "database error")
+            } else {
+                self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+            }
+            
+        }
+        
+    }
 }
 
