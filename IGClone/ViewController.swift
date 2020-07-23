99ac4e010f7a91b0839aff3f313a8dffd09f05de
dotDustin <logins.dmahone@gmail.com>
2020-07-22 22:24:40 -0400

    Initial Commit

---
+++ IGClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IGClone
+//
+//  Created by Dustin Mahone on 7/22/20.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
